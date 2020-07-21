2cccd7304a6e8eb9d55c9e199a22567e71424093
Philipp Grete <grete@pa.msu.edu>
2020-07-21 12:09:30 -0400

    Add kernel using scratch

--- src/threads_and_streams/main.cpp
+++ src/threads_and_streams/main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 
 #include "Kokkos_Layout.hpp"
+#include "Kokkos_Parallel.hpp"
 #include "space_instances.hpp"
 
 using std::cout;
@@ -11,18 +12,24 @@ using DevMemSpace = Kokkos::DefaultExecutionSpace::memory_space;
 using HostMemSpace = Kokkos::HostSpace;
 using DevExecSpace = Kokkos::DefaultExecutionSpace;
 using ScratchMemSpace = DevExecSpace::scratch_memory_space;
-using MyView = Kokkos::View<double ****, Kokkos::LayoutRight, DevMemSpace>;
 
-constexpr int nghost = 2;
+using View4D = Kokkos::View<double ****, Kokkos::LayoutRight, DevMemSpace>;
+using ScratchPad2D = Kokkos::View<double **, Kokkos::LayoutRight, ScratchMemSpace,
+                                  Kokkos::MemoryTraits<Kokkos::Unmanaged>>;
+
+using team_policy = Kokkos::TeamPolicy<>;
+using team_mbr_t = Kokkos::TeamPolicy<>::member_type;
 
 auto main(int argc, char *argv[]) -> int {
-  int num_threads = 1;  // number of host threads to use
-  int num_streams = 1;  // number of (Cuda) streams to use
-  int num_cycles = 100; // number of cycles to run
-  int num_stages = 2;   // number of stages per cycles
-  int size_mesh = 256;  // size of the entire mesh
-  int size_block = 128; // size of blocks the create the mesh
-  int num_vars = 7;     // number of variables to work on
+  int num_threads = 1;         // number of host threads to use
+  int num_streams = 1;         // number of (Cuda) streams to use
+  int num_cycles = 100;        // number of cycles to run
+  int num_stages = 2;          // number of stages per cycles
+  int size_mesh = 256;         // size of the entire mesh
+  int size_block = 128;        // size of blocks the create the mesh
+  const int nghost = 2;        // number of ghost zones
+  const int num_vars = 7;      // number of variables to work on
+  const int scratch_level = 1; // 0 is actual scratch (tiny); 1 is HBM
 
   // Read command line arguments.
   for (int i = 1; i < argc; i++) {
@@ -47,17 +54,17 @@ auto main(int argc, char *argv[]) -> int {
     exit(1);
   }
 
-  int num_blocks = // total number of blocks
+  const int num_blocks = // total number of blocks
       (size_mesh / size_block) * (size_mesh / size_block) * (size_mesh / size_block);
 
   Kokkos::initialize(argc, argv);
   {
     // Allocate data
-    std::vector<MyView> views;
-    views.reserve(num_blocks);
+    std::vector<View4D> views;
     for (auto i = 0; i < num_blocks; i++) {
-      views.emplace_back("view_" + std::to_string(i), num_vars, size_block + 2 * nghost,
-                         size_block + 2 * nghost, size_block + 2 * nghost);
+      views.push_back(View4D("view_" + std::to_string(i), num_vars,
+                             size_block + 2 * nghost, size_block + 2 * nghost,
+                             size_block + 2 * nghost));
     }
 
     std::vector<DevExecSpace> exec_spaces;
@@ -70,9 +77,7 @@ auto main(int argc, char *argv[]) -> int {
       exec_spaces.push_back(DevExecSpace());
     }
 
-  int complete_cnt = 0;
-  auto f = [&](int thread_id, int num_threads) {
-    while (complete_cnt != num_blocks) {
+    auto f = [&](int thread_id, int num_threads) {
       for (auto i = 0; i < num_blocks; ++i) {
         // Workaround to ensure that the same thread works on the same MeshBlocks within
         // a cycle. Trying to circumvent problem in setting scratch pad memory, which is
@@ -80,30 +85,66 @@ auto main(int argc, char *argv[]) -> int {
         if (i % num_threads != thread_id) {
           continue;
         }
+        cout << "Thread " << thread_id << " uses stream " << i % num_streams
+             << " for: block " << i << std::endl
+             << std::flush;
 
         // in principle here's a call to a task list with multiple tasks for each block
         // now it's just a single simple kernel
+        size_t scratch_size_in_bytes = ScratchPad2D::shmem_size(num_vars, size_block);
+        auto this_block = views[i];
+        Kokkos::parallel_for(
+            "simple kernel",
+            team_policy(exec_spaces[i % num_streams], size_block * size_block,
+                        Kokkos::AUTO)
+                .set_scratch_size(scratch_level,
+                                  Kokkos::PerTeam(2 * scratch_size_in_bytes)),
+            KOKKOS_LAMBDA(team_mbr_t team_member) {
+              const int k = team_member.league_rank() / size_block + nghost;
+              const int j = team_member.league_rank() % size_block + nghost;
 
-            cout << "[" << thread_id << "] taking care of block " << i << std::endl << std::flush;
-              Kokkos::atomic_increment(&complete_cnt);
+              ScratchPad2D scratch_l(team_member.team_scratch(scratch_level), num_vars,
+                                     size_block);
+              ScratchPad2D scratch_r(team_member.team_scratch(scratch_level), num_vars,
+                                     size_block);
+
+              Kokkos::parallel_for(
+                  Kokkos::TeamVectorRange(team_member, nghost, size_block + nghost),
+                  [&](const int i) {
+                    // do sth with the vars
+                    for (auto n = 0; n < num_vars; n++) {
+                      scratch_l(n, i) = this_block(n, k, j, i - 1);
+                      scratch_r(n, i) = this_block(n, k, j, i + 1);
+                    }
+                  });
+              // Sync all threads in the team so that scratch memory is consistent
+              team_member.team_barrier();
+              Kokkos::parallel_for(
+                  Kokkos::TeamVectorRange(team_member, nghost, size_block + nghost),
+                  [&](const int i) {
+                    // do sth with the vars
+                    for (auto n = 0; n < num_vars; n++) {
+                      this_block(n, k, j, i) =
+                          scratch_l(n, i) + scratch_r(n, i) + scratch_l(0, i);
+                    }
+                  });
+            });
+      }
+    };
+
+    for (auto cycle = 0; cycle < num_cycles; cycle++) {
+      cout << "Staring cycle " << cycle << std::endl;
+      for (auto stage = 0; stage < num_stages; stage++) {
+#ifdef KOKKOS_ENABLE_OPENMP
+        // using a fixed number of partitions (= nthreads) with each partition of size 1,
+        // i.e., one thread per partition and this thread is the master thread
+        Kokkos::OpenMP::partition_master(f, num_threads, 1);
+#else
+        f(0, 1);
+#endif
+        Kokkos::fence();
       }
     }
-  };
-
-for (auto cycle = 0; cycle < num_cycles; cycle++) {
-    cout << "Staring cycle " << cycle << std::endl;
-  for (auto stage = 0; stage < num_stages; stage++) {
-#ifdef KOKKOS_ENABLE_OPENMP
-  // using a fixed number of partitions (= nthreads) with each partition of size 1,
-  // i.e., one thread per partition and this thread is the master thread
-  Kokkos::OpenMP::partition_master(f, num_threads, 1);
-#else
-  f(0, 1);
-#endif
-Kokkos::fence();
-complete_cnt = 0;
-  }
-}
     if (num_streams > 1) {
       for (auto n = 0; n < num_streams; n++) {
         SpaceInstance<DevExecSpace>::destroy(exec_spaces[n]);
